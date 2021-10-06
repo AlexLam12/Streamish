d50601d1c907d5249ac60e398847d8769407f594
AlexLam12 <Alex.e015@gmail.com>
2021-10-06 15:49:39 -0500

    first

---
+++ Streamish/Repositories/IVideoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Streamish.Models;
+
+namespace Streamish.Repositories
+{
+    public interface IVideoRepository
+    {
+        void Add(Video video);
+        void Delete(int id);
+        Video GetById(int id);
+        List<Video> GetAll();
+        void Update(Video video);
+        List<Video> GetAllWithComments();
+        List<Video> Search(string q, bool sortDesc);
+    }
+}
