cb402f9410bc22745047f9ed5ed3d36eec2e3ef4
AlexLam12 <Alex.e015@gmail.com>
2021-10-08 15:33:21 -0500

    Video Form

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardBody } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Video = ({ video }) => {
   return (
@@ -12,7 +13,9 @@ const Video = ({ video }) => {
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen />
-
+        <Link to={`/videos/${video.id}`}>
+            <strong>{video.title}</strong>
+        </Link> 
         <p>
           <strong>{video.title}</strong>
         </p>
